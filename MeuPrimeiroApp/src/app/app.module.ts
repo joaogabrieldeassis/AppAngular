3d493ebb9ef680d4cbf4409c479ca7dda418cf05
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-10-06 15:25:48 -0300

    Primeiro componente adicionado

--- MeuPrimeiroApp/src/app/app.module.ts
+++ MeuPrimeiroApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JoaoTesteComponent } from '../joao-teste/joao-teste.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JoaoTesteComponent
   ],
   imports: [
     BrowserModule,
---
+++ MeuPrimeiroApp/src/joao-teste/joao-teste.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-joao-teste',
+  templateUrl: './joao-teste.component.html',
+  styleUrls: ['./joao-teste.component.css']
+})
+export class JoaoTesteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
