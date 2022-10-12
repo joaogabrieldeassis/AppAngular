0aa73c7efd594e9d68c26259807d861cc5aa1dc2
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-10-12 08:58:00 -0300

    Criando os components

--- MyStore/src/app/app.module.ts
+++ MyStore/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
 import { HomeComponent } from './navegacao/home/home.component';
 import { RodapeComponent } from './navegacao/rodape/rodape.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     HomeComponent,
-    RodapeComponent
+    RodapeComponent,
+    SobreComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule
