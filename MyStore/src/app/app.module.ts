ce47dee8bcdacfc6fff43f0972c6e1da9a0963b3
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-10-12 08:24:50 -0300

    Criando os components

--- MyStore/src/app/app.module.ts
+++ MyStore/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { RodapeComponent } from './navegacao/rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule
---
+++ MyStore/src/app/navegacao/rodape/rodape.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rodape',
+  template: `
+    <p>
+      rodape works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class RodapeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
